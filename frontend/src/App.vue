3abeada45b707446299ecd0d98895cfff1d098e4
vkunreal <75563669+vkunreal@users.noreply.github.com>
2022-02-20 16:39:20 +0300

    Vue added

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <TextComp msg="Hello world" />
+</template>
+
+<script>
+import TextComp from "./components/TextComp.vue";
+
+export default {
+  name: "App",
+  components: {
+    TextComp,
+  },
+};
+</script>
